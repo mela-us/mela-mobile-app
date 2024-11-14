16742fa354ca8cfa559c6b5eb0e166adb921d3f3
Tran Tien <trantien4868@gmail.com>
2024-11-14 15:55:25 +0700

    fix: change collection of ExampleMongoDBEntity

--- backend/mela/src/main/java/com/hcmus/mela/model/mongo/ExampleMongoDBEntity.java
+++ backend/mela/src/main/java/com/hcmus/mela/model/mongo/ExampleMongoDBEntity.java
@@ -2,6 +2,7 @@ package com.hcmus.mela.model.mongo;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collation = "topics")
+
+@Document(collection = "topics")
 public class ExampleMongoDBEntity {
 }
