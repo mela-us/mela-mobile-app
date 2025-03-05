f6fb3a190f4c392798ad5fa08fb5c07560404e5a
nohiup <nohiup@gmail.com>
2025-03-05 19:35:17 +0700

    new: init branch

--- frontend/mela/lib/presentation/chat/chat_screen.dart
+++ frontend/mela/lib/presentation/chat/chat_screen.dart
@@ -1,11 +1,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:mela/constants/app_theme.dart';
+import 'package:mela/constants/dimens.dart';
 
 import '../../constants/assets.dart';
+import '../../utils/locale/app_localization.dart';
 
 class ChatScreen extends StatefulWidget {
-  ChatScreen({super.key});
+  const ChatScreen({super.key});
 
   @override
   State<ChatScreen> createState() => _ChatScreenState();
@@ -26,49 +28,75 @@ class _ChatScreenState extends State<ChatScreen> {
         automaticallyImplyLeading: false,
         backgroundColor: Theme.of(context).colorScheme.appBackground,
         elevation: 0,
-        title: Text(
-          'Chat AI',
-          style: Theme.of(context).textTheme.heading.copyWith(
-            color: Theme.of(context).colorScheme.textInBg1,
-          ),
-        ),
+        title: _buildTitle(context),
       ),
       backgroundColor: Theme.of(context).colorScheme.appBackground,
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                  'Coming Soon',
-                  textAlign: TextAlign.start,
-                  style: TextStyle(
-                    fontSize: 50,
-                    fontFamily: 'Lato',
-                    fontWeight: FontWeight.bold,
-                    color: Theme.of(context).colorScheme.textInBg1,
-                  )
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+              ],
+            ),
+          )
+      ),
+    );
+  }
+
+  //Build component:------------------------------------------------------------
+  Widget _buildTitle(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: Dimens.horizontal_padding),
+      child: Row(
+        children: [
+          _buildBackButton(context),
+          Expanded(
+            child: Align(
+              alignment: Alignment.center,
+              child: Padding(
+                  padding: const EdgeInsets.only(right: 40),
+                  child: _buildTextTitle(context)
               ),
-              SizedBox(height: 10),
-              Text(
-                  'Tính năng sẽ được ra mắt trong thời gian tới!',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontFamily: 'Lato',
-                    color: Theme.of(context).colorScheme.textInBg2,
-                  )
-              ),
-              SizedBox(height: 10),
-              Image.asset(
-                Assets.currently_developing_icon,
-                width: 300,
-                height: 300,
-              ),
-            ],
+            ),
           ),
-        )
+        ],
+      ),
+    );
+  }
+
+  //Build items:----------------------------------------------------------------
+  Widget _buildBackButton(BuildContext context){
+    return GestureDetector(
+      onTap: () => {
+
+      },
+      // child: Image.asset(
+      //   Assets.arrow_back_longer,
+      //   width: 26,
+      //   height: 20,
+      // ),
+      child: SizedBox(
+        width: 26,
+        height: 26,
+        child: Icon(
+          Icons.history_rounded,
+          color: Theme.of(context).colorScheme.textInBg1,
+        ),
+      )
+    );
+  }
+
+  Widget _buildTextTitle(BuildContext context){
+    return Center(
+      child: Text(
+        "Mela AI",
+        style: Theme.of(context)
+            .textTheme
+            .heading
+            .copyWith(color: Theme.of(context).colorScheme.textInBg1),
+        textAlign: TextAlign.center,
       ),
     );
   }
