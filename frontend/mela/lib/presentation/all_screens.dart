81fcb2e71e4e7599720fec900b80c66f6b9c6916
nohiup <nohiup@gmail.com>
2025-03-29 12:11:58 +0700

    Animate: - Nav bar tab change animation

--- frontend/mela/lib/presentation/all_screens.dart
+++ frontend/mela/lib/presentation/all_screens.dart
@@ -17,7 +17,8 @@ class _AllScreensState extends State<AllScreens> {
   // Index for the currently selected tab
   final _levelStore = getIt<LevelStore>();
   int _currentIndex = 0;
-
+  int _previousIndex = -1;
+  late PageController _pageController;
   // List of screens for each tab
   final List<Widget> _screens = [
     HomeScreen(),
@@ -26,18 +27,47 @@ class _AllScreensState extends State<AllScreens> {
     PersonalScreen(),
   ];
 
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
   void onTabTapped(int index) {
     setState(() {
       //eg: turnoff wifi,have errorString in _topicStore, change other tab, then turn on wifi, go back coureses screen, it will need to set errorString to empty
       _levelStore.resetErrorString();
+
+      // _pageController.animateToPage(
+      //   index,
+      //   duration: const Duration(milliseconds: 300),
+      //   curve: Curves.easeInOut,
+      // );
+      _previousIndex = _currentIndex;
       _currentIndex = index;
+
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: _screens[_currentIndex],
+        body: AnimatedSwitcher(
+          duration: const Duration(milliseconds: 300),
+          transitionBuilder: (Widget child, Animation<double> animation) {
+            final slideDirection = _currentIndex > _previousIndex ?
+                Offset(1, 0) : Offset(-1, 0);
+
+            return SlideTransition(
+              position: Tween<Offset>(
+                begin: slideDirection,
+                end: Offset.zero,
+              ).animate(animation),
+              child: child,
+            );
+          },
+          child: _screens[_currentIndex], // Load trực tiếp màn hình đích
+        ),
         bottomNavigationBar: CustomNavigationBar(
             currentIndex: _currentIndex,
             screens: _screens,
