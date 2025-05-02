9ac1c298783e4c138b66d9767fcfa0bb2e791d83
thinant <nguynthinan@outlook.com>
2025-05-02 20:54:38 +0700

    getStreak endpoint implement

---
+++ backend/mela/src/main/java/com/hcmus/mela/streak/controller/StreakController.java
@@ -0,0 +1,44 @@
+package com.hcmus.mela.streak.controller;
+
+import com.hcmus.mela.auth.security.jwt.JwtTokenService;
+import com.hcmus.mela.streak.dto.response.GetStreakResponse;
+import com.hcmus.mela.streak.service.StreakService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+@RequestMapping("/api/streak")
+public class StreakController {
+
+    private final StreakService streakService;
+
+    private final JwtTokenService jwtTokenService;
+
+    @GetMapping(value = "")
+    @Operation(
+            tags = "Streak Service",
+            summary = "Get user's streak",
+            description = "Retrieves a user's streak and the information belonging to the streak."
+    )
+    public ResponseEntity<GetStreakResponse> getStreak(@RequestHeader("Authorization") String authorizationHeader) {
+        UUID userId = jwtTokenService.getUserIdFromAuthorizationHeader(authorizationHeader);
+
+        log.info("Getting streak for user: {}", userId);
+
+        final GetStreakResponse getStreakResponse = streakService.getStreak(userId);
+
+        return ResponseEntity.ok(getStreakResponse);
+    }
+
+    
+}
