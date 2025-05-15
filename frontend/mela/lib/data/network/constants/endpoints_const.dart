9af51c5108381777a9e44dba1e99b4b6065ddc51
TreeDude03 <phantrinhanbt@gmail.com>
2025-05-15 12:51:49 +0700

    merge sprint 24

--- frontend/mela/lib/data/network/constants/endpoints_const.dart
+++ frontend/mela/lib/data/network/constants/endpoints_const.dart
@@ -7,7 +7,7 @@ class EndpointsConst {
   // base url
   //static const String baseUrl = "http://jsonplaceholder.typicode.com";
   // static const String baseUrl = "https://api.mela.guru";
-  static const String baseUrl = "https://api.mela.guru";
+  static const String baseUrl = "https://api.dev.mela.guru";
 
   // receiveTimeout
   static const int receiveTimeout = 30000;
